c81b3f8a53fe377c39e7d4f3c4066f9c860f129c
Ryan Turner <turnerr8@wwu.edu>
2021-11-16 14:25:50 -0800

    can get top tracks from all artists, form submit does NOT work

---
+++ script.js
@@ -0,0 +1,41 @@
+//API key	0920de014dfbd71690d7f0a435b8b5b0
+//Shared secret	67d2dc6a9fed482cb2b445aff09aad5c
+
+$(document).ready(function () {
+    let artist="sufjan stevens";
+    //submit
+    
+
+    $.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0920de014dfbd71690d7f0a435b8b5b0&artist=slowthai&track=gorgeous&format=json", function(data){
+        //console.log(data);
+    });
+
+    
+    $('#artist').append(artist);
+    $.getJSON("https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+artist+"&api_key=0920de014dfbd71690d7f0a435b8b5b0&format=json", function(data){
+        let html = "";
+
+        //get track names
+        $.each(data.toptracks.track, function(i, item){
+            html+="<p>"+item.name+"</p>";
+            //console.log(item.name);
+        });
+
+        //get album
+        //I DONT THINK THIS WILL WORK :(
+        /*
+        $.each(data.toptracks.track, function (i, item) {
+            $.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0920de014dfbd71690d7f0a435b8b5b0&artist="+artist+"&track="+item.name+"&format=json", function(song){
+                //console.log(song.track.album.image[1]['#text']);
+                html+="<img src='"+song.track.album.image[1]['#text']+"'></img>";
+            });
+            //html+="<img src="+song.track.album.image[1]['#text']+"</img>";
+        });
+        */
+
+    
+        $('#result').append(html);
+
+    });
+
+});
