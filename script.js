8631b06a2ff84c31bf1c6b09e78a6739810c7df0
Ryan Turner <turnerr8@wwu.edu>
2021-11-17 11:47:44 -0800

    got search working and a array of all songs

--- script.js
+++ script.js
@@ -2,41 +2,37 @@
 //Shared secret	67d2dc6a9fed482cb2b445aff09aad5c
 
 $(document).ready(function () {
-    let artist="svdden death";
+    let artist="";
     //submit
+    $('#submit').click(function () { 
+        artist = document.getElementById("artist-input").value;
     
-   
-
-    $.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0920de014dfbd71690d7f0a435b8b5b0&artist=slowthai&track=gorgeous&format=json", function(data){
-        //console.log(data);
-    });
-
-    
-    $('#artist').append(artist);
-    $.getJSON("https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+artist+"&api_key=0920de014dfbd71690d7f0a435b8b5b0&format=json", function(data){
-        let html = "";
-
-        //get track names
-        $.each(data.toptracks.track, function(i, item){
-            html+="<p>"+item.name+"</p>";
-            //console.log(item.name);
-        });
-
-        //get album
-        //I DONT THINK THIS WILL WORK :(
-        /*
-        $.each(data.toptracks.track, function (i, item) {
-            $.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0920de014dfbd71690d7f0a435b8b5b0&artist="+artist+"&track="+item.name+"&format=json", function(song){
-                //console.log(song.track.album.image[1]['#text']);
-                html+="<img src='"+song.track.album.image[1]['#text']+"'></img>";
+        $('#artist').html(artist);
+        $.getJSON("https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+artist+"&api_key=0920de014dfbd71690d7f0a435b8b5b0&format=json", function(data){
+            let html = "";
+            let disco = [];
+            console.log(disco);
+            //get track names
+            $.each(data.toptracks.track, function(i, item){
+                disco.push(item.name)
+                html+="<li>"+item.name+"</li>";
+                //console.log(item.name);
             });
-            //html+="<img src="+song.track.album.image[1]['#text']+"</img>";
+
+            //get album
+            //I DONT THINK THIS WILL WORK :(
+            /*
+            $.each(data.toptracks.track, function (i, item) {
+                $.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0920de014dfbd71690d7f0a435b8b5b0&artist="+artist+"&track="+item.name+"&format=json", function(song){
+                    //console.log(song.track.album.image[1]['#text']);
+                    html+="<img src='"+song.track.album.image[1]['#text']+"'></img>";
+                });
+                //html+="<img src="+song.track.album.image[1]['#text']+"</img>";
+            });
+            */
+            console.log(html);
+            $("#result").html(html);
+
         });
-        */
-
-    
-        $('#result').append(html);
-
     });
-
 });
